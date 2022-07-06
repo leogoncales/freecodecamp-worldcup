185716edcade220201d8e54bb910cf265841b08c
Leonardo Lisboa <78059707+leogoncales@users.noreply.github.com>
2022-07-06 17:31:08 -0300

    Add files via upload

---
+++ insert_data.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+echo $($PSQL "truncate teams, games")
+echo $($PSQL "ALTER SEQUENCE teams_team_id_seq RESTART WITH 1");
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  #pular a primeira linha
+  if [[ $YEAR != "year" ]]
+  then
+
+  #get winner and opponent teams ids
+    WINNER_TEAM_ID=$($PSQL "select team_id from teams where name='$WINNER'")
+    OPPONENT_TEAM_ID=$($PSQL "select team_id from teams where name='$OPPONENT'")
+    if [[ -z $WINNER_TEAM_ID ]]
+    then
+      #inserir novo winner team id
+      WINNER_TEAM_ID_RESULT=$($PSQL "insert into teams (name) values ('$WINNER')")
+      if [[ $WINNER_TEAM_ID_RESULT == "INSERT 0 1" ]]
+      then
+        echo "Inserted new winner team, $WINNER"
+      fi
+    fi
+    if [[ -z $OPPONENT_TEAM_ID ]]
+    then
+      #inserir novo opponent team id
+      OPPONENT_TEAM_ID_RESULT=$($PSQL "insert into teams (name) values ('$OPPONENT')")
+      if [[ $WINNER_TEAM_ID_RESULT == "INSERT 0 1" ]]
+      then
+        echo "Inserted new OPPONENT team, $OPPONENT"
+      fi
+    fi
+
+      #selecionar novo id
+      WINNER_TEAM_ID=$($PSQL "select team_id from teams where name='$WINNER'")
+      OPPONENT_TEAM_ID=$($PSQL "select team_id from teams where name='$OPPONENT'")
+    
+    #inserir jogos 
+    GAME_ID_RESULT=$($PSQL "insert into games(year,round,winner_id,opponent_id,winner_goals,opponent_goals) values($YEAR, '$ROUND', $WINNER_TEAM_ID, $OPPONENT_TEAM_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+    if [[ $GAME_ID_RESULT == "INSERT 0 1" ]]
+    then
+      echo "Inserted game, $YEAR | $ROUND | $WINNER x $OPPONENT"
+    fi  
+  fi
+done
+
+
